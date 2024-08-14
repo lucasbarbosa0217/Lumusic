266154d9006e5d9609278708179bae74b30cd263
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-08-14 16:48:48 -0300

    main

--- src/Components/Music-Title/Music-Title.jsx
+++ src/Components/Music-Title/Music-Title.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "../../Vars.css";
 import "./Music-Title.css";
-import CoverImg from "../../assets/cover.png";
 
 const Music_Title = ({ song }) => {
 
